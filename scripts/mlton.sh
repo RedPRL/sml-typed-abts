ac074e71ba419981cc59fe3f21edcc4a61912ef1
Jon Sterling <jon@jonmsterling.com>
2016-08-30 11:46:53 -0400

    add unparser to mlton build

--- scripts/mlton.sh
+++ scripts/mlton.sh
@@ -5,5 +5,6 @@ LIBS=$(pwd)/lib
 mlton -mlb-path-var "LIBS $LIBS" -output example.out example.mlb
 mlton -mlb-path-var "LIBS $LIBS" -output abt-unparser.out abt-unparser.mlb
 mlton -mlb-path-var "LIBS $LIBS" -output abt-parser.out abt-parser.mlb
+mlton -mlb-path-var "LIBS $LIBS" -output abt-unparser.out abt-unparser.mlb
 mlton -mlb-path-var "LIBS $LIBS" -output abt-patterns.out abt-patterns.mlb
 # mlton -mlb-path-map mlb-path-map -output abt-lcs.out abt-lcs.mlb
