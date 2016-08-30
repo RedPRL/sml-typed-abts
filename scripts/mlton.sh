3956b05bb9b557fdc08a8b5295eaece861c17864
Jon Sterling <jon@jonmsterling.com>
2016-08-30 11:41:35 -0400

    fix mlton build

--- scripts/mlton.sh
+++ scripts/mlton.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-mlton -mlb-path-map mlb-path-map -output example.out example.mlb
-mlton -mlb-path-map mlb-path-map -output abt-unparser.out abt-unparser.mlb
-mlton -mlb-path-map mlb-path-map -output abt-parser.out abt-parser.mlb
-mlton -mlb-path-map mlb-path-map -output abt-patterns.out abt-patterns.mlb
+LIBS=$(pwd)/lib
+
+mlton -mlb-path-var "LIBS $LIBS" -output example.out example.mlb
+mlton -mlb-path-var "LIBS $LIBS" -output abt-unparser.out abt-unparser.mlb
+mlton -mlb-path-var "LIBS $LIBS" -output abt-parser.out abt-parser.mlb
+mlton -mlb-path-var "LIBS $LIBS" -output abt-patterns.out abt-patterns.mlb
 # mlton -mlb-path-map mlb-path-map -output abt-lcs.out abt-lcs.mlb
