c3354318e818a34171a2fae5e71d68b5ee979233
Jonathan Sterling <jon@jonmsterling.com>
2017-07-19 19:57:53 -0400

    delete unparser crap

--- scripts/mlton.sh
+++ scripts/mlton.sh
@@ -6,6 +6,5 @@ mlyacc example/example.grm
 mllex example/example.lex
 
 mlton -mlb-path-var "LIBS $LIBS" -output example.out example.mlb
-mlton -mlb-path-var "LIBS $LIBS" -output abt-unparser.out abt-unparser.mlb
 mlton -mlb-path-var "LIBS $LIBS" -output abt-json.out abt-json.mlb
 mlton -mlb-path-var "LIBS $LIBS" -output abt-machine.out abt-machine.mlb
